75742285a18c4cf0b471d8588e3871d7d51796e6
timqian <timqian92@qq.com>
2019-03-26 23:19:40 +0800

    add pro intro

--- frontend/index.jsx
+++ frontend/index.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable import/no-extraneous-dependencies */
 /* eslint-disable jsx-a11y/anchor-is-valid */
 /* eslint-disable jsx-a11y/no-static-element-interactions */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
